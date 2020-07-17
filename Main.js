d0e2a821f005269cb74068cefae8a185ae44324a
Bluzzi <bluzzigame@gmail.com>
2020-07-18 01:07:24 +0200

    First commit, the base :)

---
+++ Main.js
@@ -0,0 +1,10 @@
+const DISCORD = require("discord.js");
+global.CLIENT = new DISCORD.Client();
+
+CLIENT.on("message", (message) => {
+    if(message.content.includes("tgm")){
+        message.reply("ok");
+    }
+});
+
+CLIENT.login("NzMzODE5MzQ1OTA1NTE2NjU2.XxIsyg.Kr-Ab2hsYpfPmb_ubwunR9fDrs0");
