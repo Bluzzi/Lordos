b4514de7e15e25bb2418c5f02a275099c0b94f85
Bluzzi <bluzzigame@gmail.com>
2020-07-22 23:58:48 +0200

    Main: add event auto loader

--- Main.js
+++ Main.js
@@ -1,10 +1,14 @@
+// Create discord client :
 const DISCORD = require("discord.js");
 global.CLIENT = new DISCORD.Client({disableMentions: "true"});
 
-CLIENT.on("message", (message) => {
-    if(message.content.includes("ping")){
-        message.reply("tgm");
-    }
+// Packadges :
+const FS = require("fs");
+
+// Auto load all events :
+FS.readdirSync("./src/events/").forEach(eventName => {
+    require("./src/events/" + eventName);
 });
 
+// Connect the client :
 CLIENT.login("NzMzODE5MzQ1OTA1NTE2NjU2.XxIsyg.Kr-Ab2hsYpfPmb_ubwunR9fDrs0");
