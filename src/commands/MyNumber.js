7fb3f4d0386c6eba3bcdda060d1919d6381a5c93
Bluzzi <bluzzigame@gmail.com>
2020-07-26 00:47:18 +0200

    Add MyNumber command class

---
+++ src/commands/MyNumber.js
@@ -0,0 +1,16 @@
+const EMBED = require("../utils/Embed");
+const CHOICE_NUMBER = require("./ChoiceNumber");
+const CONSTANTS = require("../utils/Constants");
+
+class MyNumber {
+
+    static execute(args, message){
+        if(CHOICE_NUMBER.numbers[message.author.id]){
+            EMBED.send("Votre nombre est : " + CHOICE_NUMBER.numbers[message.author.id], message.channel);
+        } else {
+            EMBED.send("Utilisez la commande ``" + CONSTANTS.prefix + "choicenumber`` pour définir votre nombre !", message.channel);
+        }
+    }
+}
+
+module.exports = MyNumber;
