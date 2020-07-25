ca763ba95df84e07446f24f72b7dfd6d48dc4c84
Bluzzi <bluzzigame@gmail.com>
2020-07-26 00:47:08 +0200

    Add CreateNumber command class

---
+++ src/commands/ChoiceNumber.js
@@ -0,0 +1,21 @@
+const EMBED = require("../utils/Embed");
+const CONSTANTS = require("../utils/Constants");
+
+var numbers = {};
+
+class ChoiceNumber {
+
+    static execute(args, message){
+        if(!args[0]){
+            EMBED.send("Vous devez faire ``" + CONSTANTS.prefix + " (votre nombre)``.", message.channel);
+            return;
+        }
+
+        numbers[message.author.id] = parseInt(args[0]);
+
+        EMBED.send("Vous avez bien choisi votre nombre : " + parseInt(args[0]), message.channel);
+    }
+}
+
+module.exports = ChoiceNumber;
+module.exports.numbers = numbers;
