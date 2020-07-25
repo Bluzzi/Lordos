89f504579df2afe2803495fbb21174b159fa7939
Bluzzi <bluzzigame@gmail.com>
2020-07-25 22:45:11 +0200

    Create the first command RandomNumber !

--- src/commands/RandomNumber.js
+++ src/commands/RandomNumber.js
@@ -1,1 +1,18 @@
-//TODO
+const EMBED = require("../utils/Embed");
+const CONSTANTS = require("../utils/Constants");
+
+class RandomNumber {
+    
+    static execute(args, message){
+        if(args.length < 2 || parseInt(args[0]) > parseInt(args[1])){
+            EMBED.send("Vous devez faire ``" + CONSTANTS.prefix + "randomnumber (min) (max)``.", message.channel);
+            return;
+        }
+
+        args = args.map(num => parseInt(num));
+
+        EMBED.send(Math.floor(Math.random() * (args[1] - args[0] + 1)) + args[0], message.channel);
+    }
+}
+
+module.exports = RandomNumber;
