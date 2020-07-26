bc70e2d42927650d0f8e8f51cdf6fea4a82084f2
LoupioFR <52181828+LMGerard@users.noreply.github.com>
2020-07-26 20:45:26 +0200

    Url command upgrade

--- src/commands/Url.js
+++ src/commands/Url.js
@@ -1,74 +1,143 @@
 const EMBED = require("../utils/Embed");
 const CONSTANTS = require("../utils/Constants");
 const Embed = require("../utils/Embed");
+const { fstat } = require("fs");
 
-var links = {
-    "codingame":"https://www.codingame.com/multiplayer/clashofcode",
-    "chess":"https://lichess.org/",
-    "werewolf":"https://wolfy.fr/play",
-    "fortnite":"https://1v1.lol/",
-    "skribbl":"https://skribbl.io/",
-    "paper":"https://paper-io.com/",
-    "wikipedia":"https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard",
-    "geoguessr":"https://www.geoguessr.com/"
-}
+const FS = require("fs");
+const MP = require("html-metadata-parser") 
 
-var aliases = {
-    "codingame":["cg"],
-    "chess":["échec","cs"]
-}
+
+
 class Url {
 
     static execute(args, message){
+
+        let config = JSON.parse(FS.readFileSync(__dirname + "/../resources/configs/url.json"),{encoding:"utf8"});
         args = args.map(function(value){
-            return value.toLowerCase()
+            return value.toLowerCase();
         });
         if(!args[0]){
-            EMBED.send("Vous devez faire ``" + CONSTANTS.prefix + "url (list | url)``.", message.channel);
+            var text = config.usages.help.replace("%p%",CONSTANTS.prefix);
+            EMBED.send(text, message.channel);
             return;
         }
-
+        if(args[0] == "help"){
+            text = config.usages.commands.replace("%p%",CONSTANTS.prefix);
+            EMBED.send(text,message.channel);
+            return;
+        }
         if(args[0] == "list"){
-            text = "Voici les différents liens enregistrés:\n"
-            for(var key in links) {
-                var text = text + "\n" + " - " + key
-                if(aliases[key]){
-                    text = text + ": (" + aliases[key].join(" | ") + ")"
+            text = "Voici les différents liens enregistrés:\n";
+
+            for(var key in config.links) {
+                text = text + "\n" + " - " + key;
+                if(config.aliases[key]){
+                    text = text + ": (" + config.aliases[key].join(" | ") + ")";
                 }
             }
 
-            Embed.send(text,message.channel)
+            Embed.send(text,message.channel);
 
             return;
         }
 
-        if(args[0] == "add" && args[1]){
-            EMBED.send("Coming soon...",message.channel)
+        if(args[0] == "add" && args[1] && args[2]){
+            console.log("done")
+            if(config.links[args[1]] && args[2]!="alias"){
+                console.log("1")
+                var text = config.usages.exist_name.replace("%v%",args[1]);
+                EMBED.send(text,message.channel);
+                return;
+            }
+            console.log("1")
+            if((!args[2].startsWith("https://") && !args[2].startsWith("http://")) && args[2] != "alias"){
+                var text = config.usages.noturl;
+                EMBED.send(text,message.channel);
+                return;
+            }
+            console.log("2")
 
-            return;
-        }
-        if(args[0] == "del" && args[1]){
-            EMBED.send("Coming soon...",message.channel)
+            if(args[2] != "alias"){
+                config.links[args[1]] = args[2];
+            }
+            let error = [];
 
-            return;
-        }
 
-        if(links[args[0]]){
-            EMBED.send("Voici le lien vers **" + args[0] +"**:\n" + links[args[0]],message.channel)
+            if(args.length > 3){
+                let slicedArgs = args.slice(3);
+                for(var i = 0; i < slicedArgs.length; i++){
+                    for(var key in config.aliases){
+                        if(config.aliases[key].includes(slicedArgs[i])){
+                            error.push(false);
+                        }
+                        else{
+                            error.push(true);
+                        }
+                    }
+                    if(error.every((currentValue) => currentValue == true)){
+                        if(!config.aliases[args[1]]){
+                            config.aliases[args[1]]=[];
+                        }
+                        config.aliases[args[1]].push(slicedArgs[i]);
+                    }
+                }
+            }
+
+            FS.writeFile(__dirname + "/../resources/configs/url.json", JSON.stringify(config, null, 4), (err) => {
+                if(err) return console.log(err);
+            });
             
+            error = error.filter((bool) => !bool).length;
+
+            EMBED.send("Done with " + error + " bad alias.", message.channel)
+
+            return;
+        }
+
+        if(args[0] == "del" && args[1]){
+            if(!config.links[args[1]]){
+                text = config.usages.existnot.replace("%v%",args[1])
+                EMBED.send(text,message.channel)
+                return;
+            }
+            if(args[2] && args[2] == "alias"){
+                delete config.aliases[args[1]]
+            }
+            else{
+                delete config.links[args[1]]
+            }
+        
+            FS.writeFile(__dirname + "/../resources/configs/url.json", JSON.stringify(config, null, 4), (err) => {
+                if(err) return console.log(err);
+            });
+
+            EMBED.send("Done",message.channel)
+
+            return;
+        }
+
+        if(config.links[args[0]]){
+            let text = args[0].charAt(0).toUpperCase() + args[0].slice(1);
+            MP.parser(config.links[args[0]], (error,result) => {
+                EMBED.send("__**[" + text + "](" + config.links[args[0]] + ")**__\n\n" + result["meta"]["description"], message.channel);
+            })
             return;        
         }
-        for(var key in aliases){
-
-
-            if(aliases[key].includes(args[0])){
-                EMBED.send("Voici le lien vers **" + key +"**:\n" + links[key],message.channel)
+        for(var key in config.aliases){
+            if(config.aliases[key].includes(args[0])){
+                let text = key.charAt(0).toUpperCase() + key.slice(1);
+                MP.parser(config.links[key], (error,result) => {
+                    EMBED.send("__**[" + text + "](" + config.links[key] + ")**__\n\n" + result["meta"]["description"], message.channel);
+                })
                 return;
             }
         }
-        Embed.send("Le lien nommé **" + args[0] + "** n'est pas enregistré.",message.channel)
+        
+        Embed.send("Le lien nommé **" + args[0] + "** n'est pas enregistré.",message.channel);
         
     }
+
+
 }
 
 module.exports = Url;
