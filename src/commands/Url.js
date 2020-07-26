5632d7ddd70aea18e4ffee90a75e07724e81f01e
LoupioFR <52181828+LMGerard@users.noreply.github.com>
2020-07-26 10:13:15 +0200

    Update Url.js
    -better >url list
    -use of aliases

--- src/commands/Url.js
+++ src/commands/Url.js
@@ -13,18 +13,28 @@ var links = {
     "geoguessr":"https://www.geoguessr.com/"
 }
 
+var aliases = {
+    "codingame":["cg"],
+    "chess":["échec","cs"]
+}
 class Url {
 
     static execute(args, message){
+        args = args.map(function(value){
+            return value.toLowerCase()
+        });
         if(!args[0]){
             EMBED.send("Vous devez faire ``" + CONSTANTS.prefix + "url (list | url)``.", message.channel);
             return;
         }
 
-        if(args[0].toLowerCase() == "list"){
+        if(args[0] == "list"){
             text = "Voici les différents liens enregistrés:\n"
             for(var key in links) {
                 var text = text + "\n" + " - " + key
+                if(aliases[key]){
+                    text = text + ": (" + aliases[key].join(" | ") + ")"
+                }
             }
 
             Embed.send(text,message.channel)
@@ -32,13 +42,32 @@ class Url {
             return;
         }
 
-        if(!links[args[0]]){
-            Embed.send("Le lien nommé **" + args[0] + "** n'est pas enregistré.",message.channel)
+        if(args[0] == "add" && args[1]){
+            EMBED.send("Coming soon...",message.channel)
 
             return;
         }
+        if(args[0] == "del" && args[1]){
+            EMBED.send("Coming soon...",message.channel)
+
+            return;
+        }
+
+        if(links[args[0]]){
+            EMBED.send("Voici le lien vers **" + args[0] +"**:\n" + links[args[0]],message.channel)
+            
+            return;        
+        }
+        for(var key in aliases){
+
+
+            if(aliases[key].includes(args[0])){
+                EMBED.send("Voici le lien vers **" + key +"**:\n" + links[key],message.channel)
+                return;
+            }
+        }
+        Embed.send("Le lien nommé **" + args[0] + "** n'est pas enregistré.",message.channel)
         
-        EMBED.send("Voici le lien vers **" + args[0] +"**:\n" + links[args[0]],message.channel)
     }
 }
 
