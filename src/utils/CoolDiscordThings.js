f4cb12c8385b0d529e954ea6e4ae2c742d957e49
Bluzzi <bluzzigame@gmail.com>
2020-07-27 15:45:38 +0200

    Create CoolDiscordThings class with two cool function :p

---
+++ src/utils/CoolDiscordThings.js
@@ -0,0 +1,33 @@
+const FUNCTION_PLUS = require("./FunctionPlus");
+
+class CoolDiscordThings {
+
+    static messageToEmoji(message){
+        return this.messageToEmojiArray(message).join("");
+    }
+
+    static messageToEmojiArray(message){
+        message = message.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+        let alphabet = FUNCTION_PLUS.characterRange("a", "z");
+        let numbers = FUNCTION_PLUS.range(0, 9).map(num => num.toString());
+        
+        let numbersName = [
+            "zero", "one", "two", "three", "four", 
+            "five", "six", "seven", "eight", "nine"
+        ];
+
+        return message.split("").map(function(char){
+            char = char.toLowerCase();
+
+            if(alphabet.includes(char)) return ":regional_indicator_" + char + ":";
+            if(numbers.includes(char)) return ":" + numbersName[char] + ":";
+            if(char === " ") return " ";
+            if(char === "\n") return "\n";
+
+            return "**" + char + "**";
+        });
+    }
+}
+
+module.exports = CoolDiscordThings;
