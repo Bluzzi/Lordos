ebce96ee6768e1d442476a7c7a195791504302a3
Bluzzi <bluzzigame@gmail.com>
2020-08-03 23:17:23 +0200

    Precision (command): update for user experience

--- src/commands/list/Precision.js
+++ src/commands/list/Precision.js
@@ -1,30 +1,36 @@
 const DISCORD = require("discord.js");
 const COMMAND = require("../Command");
 const EMBED = require("../../utils/Embed");
-const PING = require("ping");
 const FS = require("fs");
 const CANVAS = require("canvas");
-const COOL_DISCORD_THINGS = require("../../utils/CoolDiscordThings");
+
+let playingChannels = {};
 
 class Precision extends COMMAND {
 
     constructor(){
         super(
             "precision", 
-            "(le nombre de tour que vous allez devoir réecrire, par default : 1)",
+            "",
             "Un jeu de rapidité ou toutes les personnes présentes dans le salon doivent réecrire le plus rapidement possible des séries de mots.",
             []
         );
     }
     
     async execute(args, message){
+        if(!playingChannels[message.guild.id]) playingChannels[message.guild.id] = [];
+        
+        // Check if the channel is blocked :
+        if(playingChannels[message.guild.id].includes(message.channel.id)){
+            EMBED.send("Une partie est déjà en cours dans ce salon.", message.channel);
+            return;
+        }
+
+        // Blocking the living room for the time of the game :
+        playingChannels[message.guild.id].push(message.channel.id);
+
         // Read the french words list :
         let allWords = FS.readFileSync(__dirname + "/../../../resources/configs/liste_francais.txt", "utf8").split(":");
-
-        // Set the number of turns :
-        let maxTurns = 1;
-
-        if(args[0] && !isNaN(args[0])) maxTurns = args[0];
 
         // Shuffle all words :
         allWords = allWords.sort(() => Math.random() - 0.5); 
@@ -32,58 +38,56 @@ class Precision extends COMMAND {
         // Get 3 random word per turn :
         let words = [];
 
-        for(let i = 0; i < (3 * maxTurns); i++) words.push(allWords.shift());
+        for(let i = 0; i < 3; i++) words.push(allWords.shift());
 
         // Print the 3 2 1 Go :
-        let steps = ["Début dans 3", "Début dans 2", "Début dans 1", "Go !"];
+        let steps = ["Début dans 3", "Début dans 2", "Début dans 1", "Go ! Vous devez réécrire ces 3 mots le plus rapidement possible :"];
         let time = 1000;
 
-        message.channel.send(steps.shift()).then(msg => {
+        let embed = new DISCORD.MessageEmbed();
+
+        embed.setColor(EMBED.COLOR_GREEN);
+        embed.setDescription(steps.shift());
+
+        message.channel.send(embed).then(msg => {
             steps.forEach(step => {
                 time += 1000;
 
-                setTimeout(() => msg.edit(steps.shift()), time);
+                setTimeout(() => {
+                    embed.setDescription(steps.shift());
+                    msg.edit(embed);
+                }, time);
             });
         });
 
-        setTimeout(() => this.playing(args, message, words, maxTurns), 5000);
+        setTimeout(() => this.playing(message.channel, words), 5000);
     }
 
-    playing(args, message, words, maxTurns){
-        let turn = 1;
+    playing(channel, words){
+        let sentence = words.join(" ");
 
-        // Get three words for the current turn :
-        let turnWords = [];
+        // Create and send the image with 3 words :
+        channel.send(this.createImage(sentence));
 
-        for(let i = 0; i < 3; i++) turnWords.push(words.shift());
-        
-        // Create and send the image :
-        let id = ""
-        message.channel.send(this.createImage(turnWords.join(" "), turn)).then((msg) => id = msg.id);
-        
-        setTimeout(() => {
-            let winner = ""
-            message.channel.messages.fetch({ after : id}).then(messages => messages.forEach(msg =>{
-                let last = ""
-                if(msg.content != turnWords.join(" ")){
-                    msg.delete();
-                }
-                else{
-                    winner = msg.author
-                    if(last != ""){
-                        last.delete();
-                    }
-                    last = msg
-                }
-            })).then(() => {
-                if(winner != ""){
-                    EMBED.send("<@" + winner + "> a gagné !", message.channel)
-                }
-                else{
-                    EMBED.send("Personne n'a gagné !", message.channel)
-                }
-            })
-        },20000)
+        // Create message collector :
+        let collector = channel.createMessageCollector(msg => msg.content === sentence, {time: 60000});
+
+        // Check for winner :
+        collector.on("collect", msg => {
+            EMBED.send("<@" + msg.author.id + "> a gagné !", channel);
+
+            // Close the collector :
+            collector.stop();
+        });
+
+        collector.on("end", (collected, reason) => {
+            if(reason === "time") EMBED.send("Personne n'a gagné !", channel);
+
+            // Remove blocking of the channel :
+            let index = playingChannels[channel.guild.id].indexOf(channel.id);
+
+            if(index > -1) playingChannels[channel.guild.id].splice(index, 1);
+        });
     }
     
     createImage(text){
