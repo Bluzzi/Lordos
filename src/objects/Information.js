b0d82287dc10db5fc92ff932aed8b5d6e3bd81d2
Bluzzi <bluzzigame@gmail.com>
2020-08-03 16:05:10 +0200

    Create Information object class

---
+++ src/objects/Information.js
@@ -0,0 +1,29 @@
+const EMBED = require("../utils/Embed");
+
+let separator = "\n\n";
+
+class Information {
+
+    constructor(header){
+        this.header = header + " :";
+        this.information = [];
+    }
+
+    addInformation(type, info){
+        this.information.push("**" + type + " : **" + info);
+    }
+
+    toString(){
+        let information = "**__" + this.header + "__**" + separator;
+
+        this.information.forEach(info => information += info + separator);
+
+        return information;
+    }
+
+    send(channel){
+        EMBED.send(this.toString, channel);
+    }
+}
+
+module.exports = Information;
