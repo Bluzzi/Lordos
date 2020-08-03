79ff7d653f4ef93559a2f8c73747bab44c7f6c4f
JedXDs <jedxds@outlook.com>
2020-08-03 21:13:04 +0200

    Update Embed.js
    Embeds: adding option thumbnail

--- src/utils/Embed.js
+++ src/utils/Embed.js
@@ -7,18 +7,18 @@ class Embed {
     /**
      * @param {String} message message to send
      * @param {DISCORD.Channel} channel a discord channel
-     * @param {{color: String, image: String}|String} options embed color
+     * @param {{color: String, image: String, thumbnail: String}|String} options embed color
      * @returns {void} void
      * @deprecated Options should be an object, String usage is deprecated
      */
 
-    static send(message, channel, options = {color: null, image: null}){
+    static send(message, channel, options = {color: null, image: null, thumbnail: null}){
         let embed = new DISCORD.MessageEmbed();
 
         embed.setDescription(message);
         
         // Prevent older uses:
-        if(typeof options == "string") options = {color: options, image: null};
+        if(typeof options == "string") options = {color: options, image: null, thumbnail: null};
 
         // Set default color :
         embed.setColor(COLOR_GREEN);
@@ -26,6 +26,7 @@ class Embed {
         // Set options :
         if(options["color"]) embed.setColor(options["color"]);
         if(options["image"]) embed.setImage(options["image"]);
+        if(options["thumbnail"]) embed.setThumbnail(options["thumbnail"]);
 
         return channel.send(embed);
     }
