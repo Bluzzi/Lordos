4f3d9e8701e4aa393f8d029f2ca68751aca52ca7
JedXDs <jedxds@outlook.com>
2020-08-04 16:08:47 +0200

    create ascii command class

---
+++ src/commands/list/Ascii.js
@@ -0,0 +1,22 @@
+const COMMAND = require("../Command");
+const COOL_DISCORD_THINGS = require("../../utils/CoolDiscordThings");
+const EMBED = require("../../utils/Embed");
+
+class Ascii extends COMMAND {
+
+    constructor() {
+        super(
+            "ascii",
+            "(texte)",
+             "Mettre votre texte en Ascii-Art"
+        )
+    }
+
+    async execute(args, message){
+        if (!args[0]) return false;
+        if (args.join(" ").length >= 23) return EMBED.send("Le nombre de caractère maximal est de 22", message.channel);
+        message.channel.send(COOL_DISCORD_THINGS.messageToAscii(args.join(" ")));
+    }
+}
+
+module.exports = Ascii;
