055c3f998fe48137ef8691f868b3dd2ba0d118bd
LoupioFR <52181828+LMGerard@users.noreply.github.com>
2020-08-04 14:30:30 +0200

    Encode/Decode commands

---
+++ src/commands/list/Decode.js
@@ -0,0 +1,42 @@
+const base64 = require("Base64");
+const COMMAND = require("../Command");
+const EMBED = require("../../utils/Embed");
+
+
+class Encode extends COMMAND {
+
+    constructor(){
+        super("decode", "base64 | binary", "Décoder une phrase.", []);
+    }
+
+    execute(args, message){
+        if(!args[1]) {
+            EMBED.send(this.getUsage(), message.channel);
+            console.log("bug")
+            return;
+        }
+        
+        let mode = args[0];
+        args = args.splice(1);
+        let text = ""
+        
+        switch(mode){
+            case "base64":
+                text = Buffer.from(args.join(" "), "base64").toString();
+                EMBED.send(text, message.channel)
+                break;
+            case "binary":
+                text = this.bin2Text(args.join(" "));
+                EMBED.send(text, message.channel)
+                break;
+            default:
+                EMBED.send(this.getUsage(), message.channel);
+        }
+    }
+
+    bin2Text(str){
+        return str.split(' ').map(bin => String.fromCharCode(parseInt(bin, 2))).join('');
+    }
+}
+
+module.exports = Encode;
