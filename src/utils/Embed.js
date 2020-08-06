139e04f30569a7ed07f8bb2b17e68203a861c313
Bluzzi <bluzzigame@gmail.com>
2020-08-06 13:23:39 +0200

    Embed (utils): update doc and remove older use preventer

--- src/utils/Embed.js
+++ src/utils/Embed.js
@@ -1,27 +1,21 @@
 const DISCORD = require("discord.js");
-
-const COLOR_GREEN = "#20A765";
+const COLOR = require("./Color");
 
 class Embed {
 
     /**
-     * @param {String} message message to send
+     * @param {string} message message to send
      * @param {DISCORD.Channel} channel a discord channel
-     * @param {{color: String, image: String, title: String , attachment: MessageAttachment, thumbnail: String, footer: String}|String} options embed color
+     * @param {{color: string, image: string, title: string, attachment: DISCORD.MessageAttachment, thumbnail: string, footer: string}} options embed color
      * @returns {void} void
-     * @deprecated options should be an object, String usage is deprecated
      */
-
     static send(message, channel, options = {color: null, image: null, title: null, attachment: null, thumbnail: null}){
         let embed = new DISCORD.MessageEmbed();
 
         embed.setDescription(message);
-        
-        // Prevent older uses:
-        if(typeof options == "string") options = {color: options, image: null};
 
         // Set default color :
-        embed.setColor(COLOR_GREEN);
+        embed.setColor(COLOR.GREEN);
 
         // Set options :
         if(options["title"]) embed.setTitle(options["title"]);
@@ -31,39 +25,38 @@ class Embed {
         if(options["title"]) embed.setTitle(options["title"]);
         if(options["thumbnail"]) embed.setThumbnail(options["thumbnail"]);
 
-        // A laisser en dernier : ("return")
         if(options["attachment"]){
             embed.setImage("attachment://" + options.attachment.name);
             return channel.send({ files: [options.attachment], embed: embed });
         }
         
-
-
         return channel.send(embed);
     }
 
+    /**
+     * @param {string} message message to send
+     * @param {DISCORD.Channel} channel a discord channel
+     * @param {{color: string, image: string, title: string, attachment: DISCORD.MessageAttachment, thumbnail: string, footer: string}} options embed color
+     * @returns {void} void
+     */
     static edit(messageToEdit, message, options = {color: null, image: null, title: null, thumbnail: null}){
         let embed = new DISCORD.MessageEmbed();
 
         embed.setDescription(message);
-        
-        // Prevent older uses:
-        if(typeof options == "string") options = {color: options, image: null};
 
         // Set default color :
-        embed.setColor(COLOR_GREEN);
+        embed.setColor(COLOR.GREEN);
 
         // Set options :
+        if(options["title"]) embed.setTitle(options["title"]);
+        if(options["footer"]) embed.setFooter(options["footer"]);
         if(options["color"]) embed.setColor(options["color"]);
         if(options["image"]) embed.setImage(options["image"]);
         if(options["title"]) embed.setTitle(options["title"]);
         if(options["thumbnail"]) embed.setThumbnail(options["thumbnail"]);
-
 
         return messageToEdit.edit(embed);
     }
 }
 
 module.exports = Embed;
-
-module.exports.COLOR_GREEN = COLOR_GREEN;
