b5fddbdf516a2dade00310642585afd7c4aef629
Bluzzi <bluzzigame@gmail.com>
2020-08-06 19:53:42 +0200

    Create YouTube util class

---
+++ src/utils/YouTube.js
@@ -0,0 +1,25 @@
+const YT_SEARCH = require("yt-search");
+
+class YouTube {
+
+    /**
+     * Return a video info
+     * @param {string} search 
+     * @returns {Object}
+     */
+    static searchVideo(search){
+        return new Promise((resolve, reject) => {
+            YT_SEARCH(search, (err, response) => {
+                if(err) reject(err);
+    
+                for(let key in response.videos){
+                    let currentInfo = response.videos[key];
+    
+                    if(currentInfo.type === "video") resolve(currentInfo);
+                }    
+            });
+        })
+    }
+}
+
+module.exports = YouTube;
