499df36602198f3804b26ff76bce5303bd061d5d
Bluzzi <bluzzigame@gmail.com>
2020-08-06 14:00:21 +0200

    Config (utils): update JSdoc and syntax

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -3,10 +3,22 @@ const OS = require("os");
 
 class Config {
 
+    /**
+     * Set JSON content in a configuration
+     * @param {string} path
+     * @param {Object} data
+     */
     static writeJson(path, data){
-        FS.writeFileSync(path, JSON.stringify(data, null, 4, error => { if(error) CLIENT.LOGGER.warn(error)}));  
+        FS.writeFileSync(path, JSON.stringify(data, null, 4, error => { 
+            if(error) CLIENT.LOGGER.warn(error)
+        }));  
     }
 
+    /**
+     * Get a JSON config content in object
+     * @param {string} path 
+     * @returns {Object}
+     */
     static getJson(path){
         if(!FS.existsSync(path)) this.writeJson(path, {});
 
@@ -14,16 +26,16 @@ class Config {
     }
 
     /**
-     * 
-     * @param {String} path 
-     * @param {String} data 
+     * @param {string} path 
+     * @param {string} data 
      */
-
     static writeText(path, data){
-        FS.open(path, 'a', 666, (error, id) => {
+        FS.open(path, "a", 666, (error, id) => {
             if(error) CLIENT.LOGGER.warn(error);
-            FS.write(id, data+OS.EOL, null, 'utf8', (error) => { //OS.EOL = jump line
+
+            FS.write(id, data+OS.EOL, null, "utf8", (error) => {
                 if(error) CLIENT.LOGGER.warn(error);
+
                 FS.close(id, (error) => {
                     if(error) CLIENT.LOGGER.warn(error);
                 });
