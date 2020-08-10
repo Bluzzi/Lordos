b5273b2eb136c34bf11b5bc5cbbb05e417a81f3b
JedXDs <jedxds@outlook.com>
2020-08-10 13:32:16 +0200

    Fixs

--- src/events/Message.js
+++ src/events/Message.js
@@ -2,6 +2,7 @@ const EMBED = require("../utils/Embed");
 
 MAIN.CLIENT.on("message", (message) => {
     if(message.author.bot == false) MAIN.LOGGER.info(message.author.username + " : " + message.content);
+    if (message.channel.type == "dm") return;
     if(!message.mentions.users.get(message.guild.me.id))return;
     
     let text = "Salut <@!" + message.author + ">, fais ``"+ MAIN.CONSTANTS.prefix + "help`` pour voir la liste de mes commadndes."
