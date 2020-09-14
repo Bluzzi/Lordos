723f4eacc130bca7ee9a01abdd016044f4f247ee
Bluzzi <bluzzigame@gmail.com>
2020-09-15 00:11:29 +0200

    Create Clear music command class

---
+++ src/commands/list/music/Clear.js
@@ -0,0 +1,28 @@
+const COMMAND = require("../../Command");
+const DISCORD = require("discord.js");
+const MUSIC_MANAGER = require("../../../music/MusicManager");
+const EMBED = require("../../../utils/Embed");
+
+class Clear extends COMMAND {
+
+    constructor(){
+        super("clear", "Supprime toute les musiques de la queue", "music");
+
+        this.setAliases([""]);
+        this.setPermissions([]);
+        this.setUsage("");
+    }
+
+    /**
+     * @param {string[]} args 
+     * @param {DISCORD.Message} message 
+     */
+     
+    async execute(args, message){
+        MUSIC_MANAGER.getInstance(message.guild).removeQueue();
+
+        EMBED.send("Vous avez bien supprimer toutes les musiques dans la queue !");
+    }
+}
+
+module.exports = Clear;
