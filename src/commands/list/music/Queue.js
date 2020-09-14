96c1fda1c21eef0fcd6d4f5abe0f31fa8faeee3c
Bluzzi <bluzzigame@gmail.com>
2020-09-15 00:11:09 +0200

    Create Queue music command class

---
+++ src/commands/list/music/Queue.js
@@ -0,0 +1,44 @@
+const COMMAND = require("../../Command");
+const VOICE = require("../../../music/Voice");
+const MUSIC_MANAGER = require("../../../music/MusicManager");
+const EMBED = require("../../../utils/Embed");
+const DISCORD = require("discord.js");
+
+class Queue extends COMMAND {
+
+    constructor(){
+        super("queue", "Affiche la queue", "music");
+
+        this.setAliases(["q"]);
+    }
+
+    /**
+     * @param {string[]} args 
+     * @param {DISCORD.Message} message 
+     */
+    async execute(args, message){
+        let queue = MUSIC_MANAGER.getInstance(message.guild).queue;
+
+        if(!queue.length){
+            EMBED.send("La queue ne contient aucune musique.", message.channel);
+            return;
+        }
+
+        let messageInformation = "";
+        let queuePosition = 1;
+
+        queue.forEach(music => {
+            messageInformation += queuePosition + " : " + music.title + "\n\n";
+
+            queuePosition++;
+        });
+
+        EMBED.send(
+            "```" + messageInformation + "```", 
+            message.channel, 
+            {title: "Voici les liste des musiques dans la queue"}
+        );
+    }
+}
+
+module.exports = Queue;
