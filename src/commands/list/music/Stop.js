089f27cdc70424bec2ea8a38528777e0b953297c
Bluzzi <bluzzigame@gmail.com>
2020-09-15 00:11:19 +0200

    Create Stop music command class

---
+++ src/commands/list/music/Stop.js
@@ -0,0 +1,26 @@
+const COMMAND = require("../../Command");
+const DISCORD = require("discord.js");
+const VOICE = require("../../../music/Voice");
+const EMBED = require("../../../utils/Embed");
+
+class Stop extends COMMAND {
+
+    constructor(){
+        super("stop", "", "music");
+
+        this.setAliases(["stop", "s"]);
+    }
+
+    /**
+     * @param {string[]} args 
+     * @param {DISCORD.Message} message 
+     */
+    async execute(args, message){
+        MUSIC_MANAGER.getInstance(message.guild).removeQueue();
+        VOICE.getConnection(message.guild).disconnect();
+
+        EMBED.send("Vous avez bien stopper la musique !");
+    }
+}
+
+module.exports = Stop;
