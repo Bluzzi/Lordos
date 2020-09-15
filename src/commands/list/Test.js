7d8623c0d4428fed6688d3e13caacb41a6e235fb
Bluzzi <bluzzigame@gmail.com>
2020-09-16 00:46:51 +0200

    Remove this fuck

--- src/commands/list/Test.js
+++ src/commands/list/Test.js
@@ -1,7 +1,6 @@
 const COMMAND = require("../Command");
 const EMBED = require("../../utils/Embed");
 const DISCORD = require("discord.js");
-const VOICE = require("../../utils/Voice");
 
 class Test extends COMMAND {
 
@@ -14,9 +13,7 @@ class Test extends COMMAND {
      * @param {DISCORD.Message} message 
      */
     async execute(args, message){
-        console.log(await VOICE.info("https://www.youtube.com/watch?v=tzVJPgCn-Z8"));
-
-        AXIOS.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${escape(args.join(" "))}&key=${API_KEY}`).then(async response => {
+        //TODO ...
     }
 }
 
