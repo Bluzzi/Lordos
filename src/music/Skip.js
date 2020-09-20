0c322f78e2f240f1b55828a9966e45eea1af4ec1
Bluzzi <bluzzigame@gmail.com>
2020-09-20 20:14:57 +0200

    Create Skip music class for manage the skiping when a music is finished

---
+++ src/music/Skip.js
@@ -0,0 +1,43 @@
+const DISCORD = require("discord.js");
+const MUSIC_MANAGER = require("./MusicManager");
+const VOICE = require("./Voice");
+const EMBED = require("../utils/Embed");
+
+class Skip {
+
+    /**
+     * @param {DISCORD.Guild} guild 
+     * @param {DISCORD.Channel} channel
+     */
+    constructor(guild, channel){
+        VOICE.getConnection(guild).dispatcher.on("finish", (info) => {
+            // Check if a next song exist :
+            if(MUSIC_MANAGER.getInstance(guild).queue.length === 0 && !MUSIC_MANAGER.getInstance(guild).loop){
+                VOICE.getConnection(guild).disconnect();
+                return;
+            }
+
+            // Play the next song :
+            let nextSong;
+
+            if(MUSIC_MANAGER.getInstance(guild).loop){
+                nextSong = MUSIC_MANAGER.getInstance(guild).nowPlaying;
+            } else {
+                nextSong = MUSIC_MANAGER.getInstance(guild).removeFromQueue(0);
+            }
+
+            MUSIC_MANAGER.getInstance(guild).play(VOICE.getConnection(guild), nextSong);
+
+            EMBED.send(
+                "**LECTURE - **[" + nextSong.title + "](" + nextSong.url + ")",
+                channel,
+                {image: nextSong.thumbnail}
+            );
+
+            // Restart finish checker :
+            new Skip(guild, channel);
+        });
+    }
+}
+
+module.exports = Skip;
