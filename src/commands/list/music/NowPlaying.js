053c269078f843bc6071a1dd5061e134fddc33fa
Bluzzi <bluzzigame@gmail.com>
2020-09-20 20:14:24 +0200

    NowPlaying (music command): finish the command

--- src/commands/list/music/NowPlaying.js
+++ src/commands/list/music/NowPlaying.js
@@ -1,7 +1,9 @@
 const COMMAND = require("../../Command");
 const DISCORD = require("discord.js");
 const MUSIC_MANAGER = require("../../../music/MusicManager");
+const VOICE = require("../../../music/Voice");
 const EMBED = require("../../../utils/Embed");
+const FUNCTION_PLUS = require("../../../utils/FunctionPlus");
 
 class NowPlaying extends COMMAND {
 
@@ -17,10 +19,38 @@ class NowPlaying extends COMMAND {
      */
     async execute(args, message){
         let musicInfo = MUSIC_MANAGER.getInstance(message.guild).nowPlaying;
+        let dispatcher = VOICE.getConnection(message.guild).dispatcher;
 
-        if(musicInfo){
+        if(musicInfo && dispatcher){
+            // Play or pause indicator :
+            let timeFormatted = dispatcher.paused ? "⏸️ " : "▶️ ";
+
+            let timePlayed = dispatcher.totalStreamTime - dispatcher.pausedTime;
+            let musicDuration =  musicInfo.duration.seconds * 1000;
+
+            // Duration bar :
+            for(let i = 1; i <= 20; i++){
+                timeFormatted += Math.round(timePlayed / musicDuration * 20) === i ? "🔵" : "➖";
+            }
+
+            timeFormatted += " ";
+
+            // Duration time :
+            timePlayed = FUNCTION_PLUS.convertMS(timePlayed);
+            musicDuration =  FUNCTION_PLUS.convertMS(musicDuration);
+
+            if(timePlayed.hour > 0) timeFormatted += timePlayed.hour + "h ";
+            if(timePlayed.minute > 0) timeFormatted += timePlayed.minute + "m ";
+            timeFormatted += timePlayed.second + "s ";
+
+            timeFormatted += "/ ";
+
+            if(musicDuration.hour > 0) timeFormatted += musicDuration.hour + "h ";
+            timeFormatted += musicDuration.minute + "m ";
+            timeFormatted += musicDuration.second + "s ";
+
             EMBED.send(
-                "**EN LECTURE - **[" + musicInfo.title + "](" + musicInfo.url + ")",
+                "**EN LECTURE - **[" + musicInfo.title + "](" + musicInfo.url + ")\n\n" + timeFormatted,
                 message.channel
             );
         } else {
