a2f2a2a1635cb3a4f79d3d1d2143818629bf6271
Bluzzi <bluzzigame@gmail.com>
2020-09-20 20:15:14 +0200

    FunctionPlus (utils): add convertMS() method

--- src/utils/FunctionPlus.js
+++ src/utils/FunctionPlus.js
@@ -7,6 +7,30 @@ class FunctionPlus {
     static characterRange(startChar, endChar){
         return String.fromCharCode(...this.range(startChar.charCodeAt(0), endChar.charCodeAt(0))).split("");
     }
+
+    /**
+     * Return object with day, hour, minute and seconds.
+     * @param {int} milliseconds 
+     * @returns {{day: int, hour: int, minute: int, second: int}}
+     */
+    static convertMS(milliseconds){
+        let day, hour, minute, second;
+
+        second = Math.floor(milliseconds / 1000);
+        minute = Math.floor(second / 60);
+        second = second % 60;
+        hour = Math.floor(minute / 60);
+        minute = minute % 60;
+        day = Math.floor(hour / 24);
+        hour = hour % 24;
+
+        return {
+            day: day,
+            hour: hour,
+            minute: minute,
+            second: second
+        };
+    }
 }
 
 module.exports = FunctionPlus;
