bef5f16a7ea4723b0fc6ba9a9993fa16a332e4df
Bluzzi <bluzzigame@gmail.com>
2020-09-25 22:04:15 +0200

    Avatar (utils command): fix, remove filter and optimize

--- src/commands/list/utils/Avatar.js
+++ src/commands/list/utils/Avatar.js
@@ -1,7 +1,7 @@
 const COMMAND = require("../../Command");
 const EMBED = require("../../../utils/Embed");
 const DISCORD = require("discord.js");
-const ZORO = require("zoro-api");
+
 class Avatar extends COMMAND {
 
     constructor(){
@@ -15,100 +15,15 @@ class Avatar extends COMMAND {
      * @param {DISCORD.Message} message 
      */
     async execute(args, message){
+        let user = args[0] && message.mentions.users.first() ? 
+            message.mentions.users.first() : message.author;
 
-        if(!args[0]){
-            EMBED.send(
-                "",
-                message.channel,
-                {
-                    "image": message.mentions.users.first() ?
-                        message.mentions.users.first().displayAvatarURL()
-                        : message.author.displayAvatarURL()
-                }
-            );
-        }
-
-        if(args[0]){
-
-            let avatar = await message.mentions.users.first() ?
-                await message.mentions.users.first().displayAvatarURL({ size: 512 }).replace(".webp", ".png")
-                : await message.author.displayAvatarURL({ size: 512 }).replace(".webp", ".png")
-
-
-            if(args[0] == "blur"){
-                const msg = await message.channel.send("Generating ...")
-                let final = await ZORO.blur(avatar)
-                let attachment = new DISCORD.MessageAttachment(final, "final.png");
-                message.channel.send(attachment) && msg.delete()
+        EMBED.send(
+            "Voici l'avatar de <@" + user.id + "> :", message.channel,
+            {
+                image: user.displayAvatarURL()
             }
-
-            if(args[0] == "circle"){
-                const msg = await message.channel.send("Generating ...")
-                let final = await ZORO.circle(avatar)
-                let attachment = new DISCORD.MessageAttachment(final, "final.png");
-                message.channel.send(attachment) && msg.delete()
-            }
-
-            if(args[0] == "tv"){
-                const msg = await message.channel.send("Generating ...")
-                let final = await ZORO.tv(avatar)
-                let attachment = new DISCORD.MessageAttachment(final, "final.png");
-                message.channel.send(attachment) && msg.delete()
-            }
-
-            if(args[0] == "bw"){
-                const msg = await message.channel.send("Generating ...")
-                let final = await ZORO.bw(avatar)
-                let attachment = new DISCORD.MessageAttachment(final, "final.png");
-                message.channel.send(attachment) && msg.delete()
-            }
-
-            if(args[0] == "ps4"){
-                const msg = await message.channel.send("Generating ...")
-                let final = await ZORO.ps4(avatar)
-                let attachment = new DISCORD.MessageAttachment(final, "final.png");
-                message.channel.send(attachment) && msg.delete()
-            }
-
-            if(args[0] == "gay"){
-                const msg = await message.channel.send("Generating ...")
-                let final = await ZORO.gay(avatar)
-                let attachment = new DISCORD.MessageAttachment(final, "final.png");
-                message.channel.send(attachment) && msg.delete()
-            }
-
-            if(args[0] == "pixel"){
-                const msg = await message.channel.send("Generating ...")
-                let final = await ZORO.pixel(avatar)
-                let attachment = new DISCORD.MessageAttachment(final, "final.png");
-                message.channel.send(attachment) && msg.delete()
-            }
-
-            if(args[0] == "trash"){
-                const msg = await message.channel.send("Generating ...")
-                let final = await ZORO.trash(avatar)
-                let attachment = new DISCORD.MessageAttachment(final, "final.png");
-                message.channel.send(attachment) && msg.delete()
-            }
-
-            if(args[0] == "error"){
-                const msg = await message.channel.send("Generating ...")
-                let final = await ZORO.error(avatar)
-                let attachment = new DISCORD.MessageAttachment(final, "final.png");
-                message.channel.send(attachment) && msg.delete()
-            }
-
-            if(args[0] == "triggered"){
-                const msg = await message.channel.send("Generating ...")
-                let final = await ZORO.triggered(avatar)
-                let attachment = new DISCORD.MessageAttachment(final, "final.png");
-                message.channel.send(attachment) && msg.delete()
-            }
-
-
-        }
-
-
+        );
     }
 }
 
