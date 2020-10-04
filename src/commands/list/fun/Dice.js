f927795b713d2faa36d4800f6832b94929fdc8c9
LoupioFR <lmgd@outlook.com>
2020-10-04 22:42:45 +0200

    Dice update

--- src/commands/list/fun/Dice.js
+++ src/commands/list/fun/Dice.js
@@ -7,6 +7,7 @@ class Dice extends COMMAND {
 
     constructor(){
         super("dice", "Lance un dé", "fun");
+        this.setUsage("<number>")
     }
 
     /**
@@ -14,15 +15,38 @@ class Dice extends COMMAND {
      * @param {DISCORD.Message} message 
      */
     async execute(args, message){
-        let num = Math.floor(Math.random() * 6 + 1);
+        if(args[0]){       
+            let number = parseInt(args[0]) || 1;
 
-        let file = new DISCORD.MessageAttachment(__dirname + "/../../../../resources/images/dices/dice-"+ num + ".jpg", "shifumi.png");
+            if(number < 1 || number > 20) return EMBED.send("Vous devez entrer un nombre en 1 et 20", message.channel, COLOR.RED);
 
-        EMBED.send(
-            "Les dés sont jetés !",
-            message.channel, 
-            {attachment: file}
-        );
+            let dices = [];
+
+            for(let i = 0; i < number; i++) dices.push(this.roll());
+
+            let i = 1;
+
+            let total = dices.reduce((accumulator, currentValue) => accumulator + currentValue)
+
+            dices = dices.map(dice => `Dé N°${i++} -> **${dice}**`);
+            
+            
+            EMBED.send("Les dés sont jetés !\n\n" + dices.join("\n") + "\n\nTotal : **" + total + "**", message.channel);
+        } else {
+            let num = this.roll();
+
+            let file = new DISCORD.MessageAttachment(__dirname + "/../../../../resources/images/dices/dice-"+ num + ".jpg", "shifumi.png");
+
+            EMBED.send(
+                "Les dés sont jetés !",
+                message.channel, 
+                {attachment: file}
+            );
+        }
+    }
+
+    roll(){
+        return Math.floor(Math.random() * 6 + 1);
     }
 }
 
